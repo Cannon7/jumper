2d41577cd4dac6b10b062800e3a112fb1117ea4a
Cannon7 <roa22002@gmail.com>
2022-10-20 16:04:37 -0600

    validator

---
+++ validator.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace dumper;
+
+public class validator
+{
+    public string GameEnd(errors)
+    {
+
+    }
+    public string LetterCheck(letterlist, guess)
+    {
+
+    }
+    public string GuessValidate(guess, wordlist, wordprog)
+    {
+
+    }
+}
