0fd2c2dfc36c5a1683eac2abb55845218f6f63d5
choatecory <corychoate1999@gmail.com>
2022-10-20 16:01:20 -0600

    visual class added

---
+++ visual.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace dumper;
+
+public class visual
+{
+    public void parachute()
+    {
+        int error = 0;
+        string[] items = {" ___ ", "/___\\", "\\   /", " \\ /", "  0", " /|\\", " / \\"};
+        List<string> display = new List<string>();
+        
+        
+        if (error == 0) {
+            for (int i = 0, i < 8, i++)
+                Console.WriteLine(display[i + "\n"]);
+        
+        
+        
+        
+        
+        
+        //Console.WriteLine(" ___ ");
+        //Console.WriteLine("/___\\");
+        //Console.WriteLine("\\   /");
+        //Console.WriteLine(" \\ /");
+        //Console.WriteLine("  0");
+        //Console.WriteLine(" /|\\");
+        //Console.WriteLine(" / \\");
+        
+        }
+
+
+        
+
+
+    }
+}
